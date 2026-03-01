72f5936035778db09a127f8a731bb86bf157fdb7
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2026-03-01 19:11:45 +0000

    UPDATE: Move error handling to config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+const DEV_MODE = true;
+
+if (DEV_MODE) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+} else {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+}
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
+require_once 'config.php';
+
 require_once 'DayPlan.php';
 require_once 'DayOfWeek.php';
 require_once 'MealPlan.php';
 
 // TODO: Send meal plan e.g. Monday breakfast this, Monday lunch this
 // TODO: list of breakfasts and list of dinners
-// TODO: Save meal plan to "output.txt" every Friday at 16:00
+// TODO: Save meal plan to 'output.txt' every Friday at 16:00
 // TODO: EXTENSION: Some meals are split between days e.g. bolognese on Wednesday tea and Friday tea
 // TODO: EXTENSION: Email list
 // TODO: EXTENSION: Some meals need more weight so they come up more regularly - some 4x more regular, some 2x more regular, some 1x more regular
@@ -19,8 +21,8 @@ function get_random($meals): Meal
     return new Meal($meals[$randomIndex]);
 }
 
-$breakfasts = file("breakfasts.txt");
-$dinners = file("dinners.txt");
+$breakfasts = file('breakfasts.txt');
+$dinners = file('dinners.txt');
 
 $mondayPlan = new DayPlan(DayOfWeek::Monday, get_random($breakfasts), get_random($dinners), get_random($dinners));
 $tuesdayPlan = new DayPlan(DayOfWeek::Tuesday, get_random($breakfasts), get_random($dinners), get_random($dinners));
