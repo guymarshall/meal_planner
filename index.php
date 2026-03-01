48a0440d7db86e01505eae88df19d17e823d548d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2026-03-01 21:27:30 +0000

    UPDATE: Remove unnecessary or finished TODOs

--- index.php
+++ index.php
@@ -6,13 +6,9 @@ require_once 'DayPlan.php';
 require_once 'DayOfWeek.php';
 require_once 'MealPlan.php';
 
-// TODO: Send meal plan e.g. Monday breakfast this, Monday lunch this
-// TODO: list of breakfasts and list of dinners
-// TODO: Save meal plan to 'output.txt' every Friday at 16:00
 // TODO: EXTENSION: Some meals are split between days e.g. bolognese on Wednesday tea and Friday tea
-// TODO: EXTENSION: Email list
 // TODO: EXTENSION: Some meals need more weight so they come up more regularly - some 4x more regular, some 2x more regular, some 1x more regular
-// TODO: EXTENSION: each meal has ingredient list, so list of ingredients needed in email
+// TODO: EXTENSION: each meal has ingredient list, so list of ingredients needed
 
 function getRandom($meals): Meal
 {
